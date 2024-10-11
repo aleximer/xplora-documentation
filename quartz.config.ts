6564a19d28249fea80758b64ff2dec07cb4ebb4c
alexey dorofeev <alexey@maksimer.no>
2024-10-11 10:16:29 +0200

    Quartz sync: Oct 11, 2024, 10:16 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "github.com/aleximer/xplora-documentation",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
